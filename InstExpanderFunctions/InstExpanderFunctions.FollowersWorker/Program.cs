3027eedf18968180a5ae1541f48e57f021bd635b
Boris Vashov <vashovway@gmail.com>
2023-12-29 21:31:10 +0300

    Store logs in App Insights

--- InstExpanderFunctions/InstExpanderFunctions.FollowersWorker/Program.cs
+++ InstExpanderFunctions/InstExpanderFunctions.FollowersWorker/Program.cs
@@ -1,10 +1,10 @@
 using InstExpander.BusinessLogic;
-using InstExpander.BusinessLogic.Interfaces;
 using InstExpanderFunctions.FollowersWorker;
 using InstExpanderFunctions.FollowersWorker.Middlewares;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults(workerApplication =>
@@ -18,9 +18,21 @@ var host = new HostBuilder()
         services.AddScoped<FunctionConfiguration>();
         services.AddInstagramWorker();
     })
+    .ConfigureLogging(logging =>
+    {
+        // Need to remove default filter for ApplicationInsights. Because filter is Warning level.
+        // https://github.com/Azure/azure-functions-dotnet-worker/issues/1182
+        // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide#application-insights
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
+    })
     .Build();
-
-var instaApiAuthorizer = host.Services.GetRequiredService<IInstaApiAuthorizer>();
-instaApiAuthorizer.Authorize().Wait();
 
 host.Run();
